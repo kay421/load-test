6d840a5781a1909b2e4ddb4f70e9b9721ed78005
kawaihiroo <kawaihiroo@lifull.com>
2019-09-27 13:34:38 +0900

    Add script for start load test with jmx file.

---
+++ container_start_test.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+working_dir="`pwd`"
+
+#Get namesapce variable
+tenant=`awk '{print $NF}' "$working_dir/tenant_export"`
+
+jmx="$1"
+[ -n "$jmx" ] || read -p 'Enter path to the jmx file ' jmx
+
+if [ ! -f "$jmx" ];
+then
+    echo "Test script file was not found in PATH"
+    echo "Kindly check and input the correct file path"
+    exit
+fi
+
+test_name="$(basename "$jmx")"
+
+#Get Master pod details
+
+master_pod=`kubectl get po -n $tenant | grep jmeter-master | awk '{print $1}'`
+
+kubectl cp "$jmx" -n $tenant "$master_pod:/$test_name"
+
+## Echo Starting Jmeter load test
+
+kubectl exec -ti -n $tenant $master_pod -- /bin/bash /load_test "$test_name"
